ef32d97f5aa9c957c78d6155822c3ae2505cc2cc
Jai Hari Nataraj <sjaiharinataraj@gmail.com>
2025-04-05 12:10:01 +0530

    scroll indicator added

--- script.js
+++ script.js
@@ -10,7 +10,25 @@ menuIcon.addEventListener("click", () => {
 });
 
 /* Footer Section */
+
 let year = new Date();
 year = year.getFullYear();
 const footer = document.querySelector(".footer");
 footer.innerHTML = `Created by Jai Hari Nataraj | &copy; ${year} All Rights Reserved.`;
+
+//scroll indicator
+
+const proBar= document.querySelector('.pro-bar');
+
+function handleScroll() {
+  const scrollTop = document.documentElement.scrollTop; 
+  const scrollHeight = document.documentElement.scrollHeight;
+  const clientHeight = document.documentElement.clientHeight;
+
+  const scrollableHeight = scrollHeight - clientHeight;
+  const scrollPercentage = (scrollTop / scrollableHeight) * 100;
+  proBar.style.width = `${scrollPercentage}%`
+  console.log(`Scrolled: ${scrollPercentage.toFixed(2)}%`);
+}
+
+window.addEventListener("scroll", handleScroll);
