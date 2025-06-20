04eccd9955734bd0ea9fc57f6a9f43c885dbec70
Jai Hari Nataraj <sjaiharinataraj@gmail.com>
2025-06-20 11:21:30 +0530

    updated project thumbnail

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,7 +2,7 @@ export const projects = [
     {
         proName: "ROAS Insight Suite",
         video: "https://youtu.be/2T8EHB4MPZU?feature=shared",
-        thumbmail: "https://res.cloudinary.com/du6eewwsa/image/upload/fl_preserve_transparency/v1748622934/ROAS_Insight_Suite_zgpllo.jpg?_s=public-apps",
+        thumbmail: "/project thumbnails/project-thumbnail.svg",
         techStack: ["React", "Redux ToolKit", "Router", "SCSS"],
         about: "ROAS Insight Suite is a web app that delivers interactive data visualizations and advanced filtering for ad campaign analysis. It helps marketers track key metrics like Impressions, Clicks, CTR, and Spend to optimize Return on Ad Spend (ROAS).",
         preview: "https://roas-insight-suite.vercel.app/",
@@ -11,7 +11,7 @@ export const projects = [
     {
         proName: "ROAS Insight Suite",
         video: "https://youtu.be/2T8EHB4MPZU?feature=shared",
-        thumbmail: "https://res.cloudinary.com/du6eewwsa/image/upload/fl_preserve_transparency/v1748622934/ROAS_Insight_Suite_zgpllo.jpg?_s=public-apps",
+        thumbmail: "/project thumbnails/project-thumbnail.svg",
         techStack: ["React", "Redux ToolKit", "Router", "SCSS"],
         about: "ROAS Insight Suite is a web app that delivers interactive data visualizations and advanced filtering for ad campaign analysis. It helps marketers track key metrics like Impressions, Clicks, CTR, and Spend to optimize Return on Ad Spend (ROAS).",
         preview: "https://roas-insight-suite.vercel.app/",
@@ -20,7 +20,7 @@ export const projects = [
     {
         proName: "ROAS Insight Suite",
         video: "https://youtu.be/2T8EHB4MPZU?feature=shared",
-        thumbmail: "https://res.cloudinary.com/du6eewwsa/image/upload/fl_preserve_transparency/v1748622934/ROAS_Insight_Suite_zgpllo.jpg?_s=public-apps",
+        thumbmail: "/project thumbnails/project-thumbnail.svg",
         techStack: ["React", "Redux ToolKit", "Router", "SCSS"],
         about: "ROAS Insight Suite is a web app that delivers interactive data visualizations and advanced filtering for ad campaign analysis. It helps marketers track key metrics like Impressions, Clicks, CTR, and Spend to optimize Return on Ad Spend (ROAS).",
         preview: "https://roas-insight-suite.vercel.app/",
@@ -29,10 +29,38 @@ export const projects = [
     {
         proName: "ROAS Insight Suite",
         video: "https://youtu.be/2T8EHB4MPZU?feature=shared",
-        thumbmail: "https://res.cloudinary.com/du6eewwsa/image/upload/fl_preserve_transparency/v1748622934/ROAS_Insight_Suite_zgpllo.jpg?_s=public-apps",
+        thumbmail: "/project thumbnails/project-thumbnail.svg",
         techStack: ["React", "Redux ToolKit", "Router", "SCSS"],
         about: "ROAS Insight Suite is a web app that delivers interactive data visualizations and advanced filtering for ad campaign analysis. It helps marketers track key metrics like Impressions, Clicks, CTR, and Spend to optimize Return on Ad Spend (ROAS).",
         preview: "https://roas-insight-suite.vercel.app/",
         github: "https://github.com/ijaihari/ROAS-Insight-Suite"
     },
+    {
+        proName: "ROAS Insight Suite",
+        video: "https://youtu.be/2T8EHB4MPZU?feature=shared",
+        thumbmail: "/project thumbnails/project-thumbnail.svg",
+        techStack: ["React", "Redux ToolKit", "Router", "SCSS"],
+        about: "ROAS Insight Suite is a web app that delivers interactive data visualizations and advanced filtering for ad campaign analysis. It helps marketers track key metrics like Impressions, Clicks, CTR, and Spend to optimize Return on Ad Spend (ROAS).",
+        preview: "https://roas-insight-suite.vercel.app/",
+        github: "https://github.com/ijaihari/ROAS-Insight-Suite"
+    },
+    {
+        proName: "ROAS Insight Suite",
+        video: "https://youtu.be/2T8EHB4MPZU?feature=shared",
+        thumbmail: "/project thumbnails/project-thumbnail.svg",
+        techStack: ["React", "Redux ToolKit", "Router", "SCSS"],
+        about: "ROAS Insight Suite is a web app that delivers interactive data visualizations and advanced filtering for ad campaign analysis. It helps marketers track key metrics like Impressions, Clicks, CTR, and Spend to optimize Return on Ad Spend (ROAS).",
+        preview: "https://roas-insight-suite.vercel.app/",
+        github: "https://github.com/ijaihari/ROAS-Insight-Suite"
+    },
+    {
+        proName: "ROAS Insight Suite",
+        video: "https://youtu.be/2T8EHB4MPZU?feature=shared",
+        thumbmail: "/project thumbnails/project-thumbnail.svg",
+        techStack: ["React", "Redux ToolKit", "Router", "SCSS"],
+        about: "ROAS Insight Suite is a web app that delivers interactive data visualizations and advanced filtering for ad campaign analysis. It helps marketers track key metrics like Impressions, Clicks, CTR, and Spend to optimize Return on Ad Spend (ROAS).",
+        preview: "https://roas-insight-suite.vercel.app/",
+        github: "https://github.com/ijaihari/ROAS-Insight-Suite"
+    },
+    
 ];
