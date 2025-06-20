4dded0356115ee1084b3688a25d5eae2c59db66d
Jai Hari Nataraj <sjaiharinataraj@gmail.com>
2025-06-20 17:51:43 +0530

    responsive contact section

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,7 @@
 import '../styles/contact.scss';
 import { FaLinkedin, FaGithub, FaXTwitter, FaInstagram } from "react-icons/fa6";
 
-function Contact() {
+function Contact() { 
     return (
         <div className="section">
             <h1 className="title">Let's Connect !</h1>
