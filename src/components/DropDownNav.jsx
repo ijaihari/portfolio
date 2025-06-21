43d69f01039ed2e0b16f5d27d831c8cdabd156d3
Jai Hari Nataraj <sjaiharinataraj@gmail.com>
2025-06-21 13:17:04 +0530

    applied backdrop for nav and dropdown

--- src/components/DropDownNav.jsx
+++ src/components/DropDownNav.jsx
@@ -1,16 +1,17 @@
-import '../styles/dropdown.scss';
-
-function DropDownNav({ onClose }) {
-  return (
-    <div className="dropdown-menu">
-      <a href="#hero" onClick={onClose}>Home</a>
-      <a href="#about" onClick={onClose}>About</a>
-      <a href="#skills" onClick={onClose}>Skills</a>
-      <a href="#projects" onClick={onClose}>Projects</a>
-      <a href="#achiev" onClick={onClose}>Achievements</a>
-      <a href="#contact" onClick={onClose}>Contact</a>
-    </div>
-  );
+import ReactDOM from 'react-dom';
+import '../styles/dropdown.scss'
+function Dropdown({ onClose }) {
+    return ReactDOM.createPortal(
+        <div className="dropdown-menu">
+            <a href="#hero" onClick={onClose}>Home</a>
+            <a href="#about" onClick={onClose}>About</a>
+            <a href="#skills" onClick={onClose}>Skills</a>
+            <a href="#projects" onClick={onClose}>Projects</a>
+            <a href="#achiev" onClick={onClose}>Achievements</a>
+            <a href="#contact" onClick={onClose}>Contact</a>
+        </div>,
+        document.body
+    );
 }
 
-export default DropDownNav;
+export default Dropdown;
